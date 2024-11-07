6fc200172be46fbb4d567d7f8d9165049d9f21d5
Cannelle <cannellou56430@gmail.com>
2024-11-07 11:10:54 +0100

    campus, ville et lieu ajoutés dans le formulaire

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -3,8 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Campus;
+use App\Entity\Lieu;
 use App\Entity\Sortie;
 use App\Entity\Ville;
+use Doctrine\DBAL\Types\FloatType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
@@ -14,6 +16,8 @@ use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use function Sodium\add;
 
@@ -41,9 +45,30 @@ class SortieType extends AbstractType
             ])
             ->add('infosSortie', TextareaType::class, [
                 'label' => 'Description et infos : ',
+            ])
+            ->add('campus', EntityType::class, [
+                'label' => 'Campus',
+                'class' => Campus::class,
+                'choice_label' => 'nom',
+                'placeholder' => '--Choisis ton campus--',
+            ])
+            ->add('ville', EntityType::class, [
+                'label' => 'Ville',
+                'class' => Ville::class,
+                'choice_label' => 'nom',
+                'mapped' => false
+            ])
+            ->add('lieu', EntityType::class, [
+            'label' => 'Lieu',
+            'class' => Lieu::class,
+            'choice_label' => 'nom',
+            'placeholder' => '--Choisis ton lieu--',
+            'mapped' => true,
+            'required' => true
             ]);
-
     }
+
+
 
     public function configureOptions(OptionsResolver $resolver): void
     {
