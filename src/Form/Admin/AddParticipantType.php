aec3c46ff96471a66da77f44cc3de3ba4642402c
Tenzin <tenzin.gadentsang2024@campus-eni.fr>
2024-11-08 13:32:28 +0100

    inscription utilisateur par admin

--- src/Form/Admin/AddParticipantType.php
+++ src/Form/Admin/AddParticipantType.php
@@ -6,13 +6,15 @@ use App\Entity\Campus;
 use App\Entity\Participant;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 
 class AddParticipantType extends AbstractType
@@ -35,19 +37,34 @@ class AddParticipantType extends AbstractType
             ->add('email', EmailType::class, [
                 'label' => 'Email'
             ])
-            ->add('plainPassword', RepeatedType::class, [
-
-                'type' => PasswordType::class,
-                'required' => false,
+            ->add('plainPassword', PasswordType::class, [
                 'mapped' => false,
-                'first_options' => ['attr' => ['placeholder' => 'Enter Your New Password']],
-
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                ],
             ])
             ->add('campus', EntityType::class, [
                 'label' => 'Campus',
                 'class' => Campus::class,
                 'choice_label' => 'nom',
             ])
+           /* ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'ROLE_USER' => 'ROLE_USER',
+                    'ROLE_ADMIN' => 'ROLE_ADMIN',
+                ],
+                'expanded' => false,
+                'multiple' => false,
+
+            ])*/
+             ->add('actif', ChoiceType::class, [
+                   'choices'  => [
+                       'Oui' => true,
+                       'Non' => false,
+                   ],
+               ])
 
             ->add('ajouter', SubmitType::class, ['label' => 'Ajouter'])
             ->add('annuler', SubmitType::class, ['label' => 'Annuler'])
